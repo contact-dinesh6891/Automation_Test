15cb692ab9d25235b27e78582c604a13dce44118
contact-dinesh6891 <69479143+contact-dinesh6891@users.noreply.github.com>
2021-01-11 22:22:30 +0400

    Retry Machenism implemented

---
+++ src/main/java/com/eVisa/qa/util/MyTransformer.java
@@ -0,0 +1,20 @@
+package com.eVisa.qa.util;
+
+import java.lang.reflect.Constructor;
+
+import org.testng.IAnnotationTransformer;
+import org.testng.annotations.ITestAnnotation;
+
+import java.lang.reflect.Method;
+
+
+public class MyTransformer implements IAnnotationTransformer {
+	public void transform(ITestAnnotation annotation,Class testClass,Constructor testConstructor,Method testMethod) {
+		annotation.setRetryAnalyzer(RetryAnalyzer.class);
+	}
+	
+
+	
+	
+	
+}
